f67e403247566424e17be9ecacdc67307746aa3f
irapanch <117355580+irapanch@users.noreply.github.com>
2024-05-22 17:21:09 +0300

    tailwind, module.css, status-lable

--- src/app/components/active-label.tsx
+++ src/app/components/active-label.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from './active-label.module.css';
 
 export interface ActiveLabelProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
+console.log(styles);
 
-export default function ActiveLabel({children}:ActiveLabelProps) {
-  return 
-    <span>{children}</span>
-  
+export default function ActiveLabel({ children }: ActiveLabelProps) {
+  return <span className={styles.label}>{children}</span>;
 }
