5d01f95f450278dcb0e6cab8918128ec709db265
irapanch <117355580+irapanch@users.noreply.github.com>
2024-05-27 15:03:07 +0300

    useRouter

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,10 +1,17 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import SidebarItem from './sidebar-item';
+import { useRouter } from 'next/navigation';
 
 export interface SidebarProps {}
 
 export default function Sidebar({}: SidebarProps) {
+
+  const router = useRouter();
+  const handleExitClick = () => {
+router.push('/')
+  }
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
@@ -32,14 +39,18 @@ export default function Sidebar({}: SidebarProps) {
             Companies
           </SidebarItem>
         </ul>
-        <button className="flex items-center gap-2 p-6 mt-auto mx-auto">
+        <button
+         onClick={handleExitClick}
+        className="flex items-center gap-2 p-6 mt-auto mx-auto">
           <Image
             width={18}
             height={18}
             src="/icons/arrow-left-on-rectangle.svg"
             alt="exit icon"
           />
-          <span className="font-medium text-white">Exit</span>
+          <span 
+         
+          className="font-medium text-white">Exit</span>
         </button>
       </div>
     </aside>
