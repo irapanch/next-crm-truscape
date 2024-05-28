b00ae911a22b22fe2225dd91b01cf3d4f09e5275
irapanch <117355580+irapanch@users.noreply.github.com>
2024-05-28 14:47:27 +0300

    Parallel Routes & error & defaultpage

---
+++ src/app/(admin)/dashboard/@sales/page.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import SummaryTable from '@/app/components/summary-table';
+import SummaryTableHeader from '@/app/components/summary-table-header';
+import SummaryTableCell from '@/app/components/summary-table-cell';
+import DashboardCard from '@/app/components/dashboard-card';
+import { getSummarySales } from '@/app/lib/api';
+import MagicButton from '@/app/components/magic-button';
+
+export interface PageProps {}
+
+interface SalesData {
+    companyId: number;
+    companyTitle: string;
+    sold: number;
+    income: number;
+  }
+
+export default async function Page({}: PageProps) {
+  const data: SalesData[] = await new Promise((res) => {
+    setTimeout(()=>{
+        res(getSummarySales());
+    }, 4000 )
+   
+  });
+
+
+  return (
+    <DashboardCard label={
+        <>
+        Sales details <MagicButton/>
+        </>
+    }>
+      <SummaryTable
+        headers={
+          <>
+            <SummaryTableHeader>Company</SummaryTableHeader>
+            <SummaryTableHeader align="center">Sold</SummaryTableHeader>
+            <SummaryTableHeader align="center">Income</SummaryTableHeader>
+          </>
+        }
+      >
+        {data.map(({ companyId, companyTitle, sold, income }) => (
+          <tr key={companyId}>
+            <SummaryTableCell>{companyTitle}</SummaryTableCell>
+            <SummaryTableCell align="center">{sold}</SummaryTableCell>
+            <SummaryTableCell align="center">{`$${income}`}</SummaryTableCell>
+          </tr>
+        ))}
+      </SummaryTable>
+    </DashboardCard>
+  );
+}
---
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import StatCard from '@/app/components/stat-card';
+import { getSummaryStats } from '@/app/lib/api';
+import { StatCardType } from '@/app/components/stat-card/stat-card';
+import Link from 'next/link';
+
+export interface PageProps {}
+
+const labelByStat = {
+  promotions: 'Total promotions',
+  categories: 'Total categories',
+  newCompanies: 'New companies',
+  activeCompanies: 'Total active companies',
+};
+
+export default async function Page({}: PageProps) {
+  const data = await getSummaryStats();
+
+  return (
+    <div className="grid grid-cols-12 gap-5">
+      {(Object.keys(labelByStat) as (keyof typeof data)[]).map((key) => (
+        <Link href={`/dashboard/${key}`} key={key} className="col-span-3">
+          <StatCard
+            type={StatCardType.Gradient}
+            label={labelByStat[key]}
+            counter={data[key]}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+}
